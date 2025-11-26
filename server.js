224bc28f4af23a4b8456f35250381b6907543f91
prachirealjava <bansalprachi908@gmail.com>
2025-11-26 13:45:54 +0530

    JSG

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get("/get-one", function (req, resp) {
 });
 
 // login------------------------------------------
-// app.get("/do-login", function (req, resp) {
+app.get("/do-login", function (req, resp) {
     let email = req.query.emailid;
     let pwd = req.query.pwd;
 
@@ -131,6 +131,30 @@ app.get("/get-one", function (req, resp) {
             }
         }
     );
+});
+
+
+
+
+
+
+// app.get("/do-login", function (req, resp) {
+//     let email = req.query.emailid;
+//     let pwd = req.query.pwd;
+
+//     mySqlVen.query("SELECT * FROM user WHERE emailid=? AND pwd=?", [email, pwd], function (errKuch, allRecords) {
+//      if (allRecords.length == 0) {
+//         resp.send("Invalid");
+//     }
+//     else if (allRecords[0].status == 1) {
+    
+//         resp.send(allRecords[0].usertype);
+//     }
+//     else {
+        
+//         resp.send("Blocked"); 
+//     }
+// });
 
 
 //       mySqlVen.query("select * from user where emailid=? and pwd=?", [email, pwd], function (errKuch, allRecords) {
